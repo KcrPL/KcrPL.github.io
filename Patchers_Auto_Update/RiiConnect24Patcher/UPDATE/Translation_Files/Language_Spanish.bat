7dc75e6b7de4f4b6af0fad193f8ee3df7f47e7d5
KcrPL <kcrplkcrpl@gmail.com>
2020-09-12 10:42:11 +0200

    New translations README.txt (Spanish)

---
+++ Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_Spanish.bat
@@ -0,0 +1,6 @@
+DO NOT TRANSLATE, ONLY READ.
+
+Hola. Por favor ejecuta cmd.exe en tu computadora, escribe lo siguiente y dale a INTRO.
+wmic os get OSLanguage /Value
+
+Por favor escribe el valor que se ha devuelto:´´3082´´: "1033"
