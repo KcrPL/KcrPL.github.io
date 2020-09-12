e9c3649fabd2942da97a62c679bdbcda7813c927
KcrPL <kcrplkcrpl@gmail.com>
2020-09-12 10:53:37 +0200

    Del this

--- Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_Portuguese, Brazilian.bat
+++
@@ -1,7 +0,0 @@
-DO NOT TRANSLATE, ONLY READ.
-
-Hello. Please run cmd.exe on your computer and type this in and hit ENTER.
-wmic os get OSLanguage /Value
-
-Please type in the value that has been returned below:
-1046
