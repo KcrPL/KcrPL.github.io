860cd407c06ba9e51780b1555acb79caa0a48533
KcrPL <kcrplkcrpl@gmail.com>
2020-09-21 22:25:02 +0200

    Test translations

---
+++ Patchers_Auto_Update/RiiConnect24Patcher_Beta/UPDATE/Update_Translations.bat
@@ -0,0 +1,15 @@
+:: remember to pip install unidecode
+
+echo off
+	rmdir /q /s Translation_Files_CHCP_OFF
+	md Translation_Files_CHCP_OFF
+::		copy "Translation_Files\*.*" "Translation_Files_CHCP_OFF\"
+			echo Working...
+			echo.
+		for /f %%f in ('dir /b Translation_Files') do (
+		echo %%f
+		if not "%%f"=="Language_ru-RU.bat" unidecode -e UTF-8 Translation_Files\%%f>>Translation_Files_CHCP_OFF\%%f
+		)
+	echo.
+	echo Done!
+	pause
