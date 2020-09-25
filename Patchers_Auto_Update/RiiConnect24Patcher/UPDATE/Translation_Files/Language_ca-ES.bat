ad9b6236470fc5c2da5a3451873e6d5bb33a9475
KcrPL <kcrplkcrpl@gmail.com>
2020-09-25 15:41:48 +0200

    New translations Language_English.bat (Catalan)

--- Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_ca-ES.bat
+++ Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_ca-ES.bat
@@ -1,6 +1,5 @@
 if "%1"=="-chcp" chcp 65001
 :: Should be available locally
-
 set string1=RiiConnect your Wii.
 set string2=Start
 set string3=Credits
@@ -507,4 +506,4 @@ set string485=SOLUTION: Please check your internet connection.
 set string486=ERROR DETAILS: Curl write error. Try moving the patcher to desktop and try again.
 set string487=SOLUTION: Please install latest .NET Framework, then try again.
 
-
+set string488=SD Card
