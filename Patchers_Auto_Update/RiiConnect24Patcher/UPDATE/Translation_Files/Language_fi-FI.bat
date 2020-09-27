f78b5948f77b7b5a6a6e038cf9bc261944549f68
KcrPL <kcrplkcrpl@gmail.com>
2020-09-27 20:00:09 +0200

    New translations Language_English.bat (Finnish)

--- Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_fi-FI.bat
+++ Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_fi-FI.bat
@@ -508,3 +508,7 @@ set string487=SOLUTION: Please install latest .NET Framework, then try again.
 
 set string488=SD Card
 set string489=Start File Explorer.
+set string490=Installation failed for WAD:
+set string491=Installation failed for:
+set string492=WAD(s)
+set string493=Pressing any key will open the error log and return to main menu.
