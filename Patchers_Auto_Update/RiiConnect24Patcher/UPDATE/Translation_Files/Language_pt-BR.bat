fc8b8062066e718453a945e4c75e41dc42803743
KcrPL <kcrplkcrpl@gmail.com>
2020-09-27 20:00:47 +0200

    New translations Language_English.bat (Portuguese, Brazilian)

--- Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_pt-BR.bat
+++ Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_pt-BR.bat
@@ -508,3 +508,7 @@ set string487=Solução: Por favor instale o .NET Framework mais recente e tente
 
 set string488=SD Card
 set string489=Start File Explorer.
+set string490=Installation failed for WAD:
+set string491=Installation failed for:
+set string492=WAD(s)
+set string493=Pressing any key will open the error log and return to main menu.
