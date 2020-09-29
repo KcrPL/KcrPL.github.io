c45342dc467561a3b611d56e1b51b1bd9ff1bb27
KcrPL <kcrplkcrpl@gmail.com>
2020-09-29 07:41:15 +0200

    New translations Language_English.bat (Russian)

--- Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_ru-RU.bat
+++ Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/Translation_Files/Language_ru-RU.bat
@@ -509,8 +509,8 @@ set string486=ДЕТАЛИ О ОШИБКЕ: Ошибка записи curl. По
 set string487=РЕШЕНИЕ: Пожалуйста установите последний .NET Framework и попробуйте ещё раз.
 
 set string488=SD-карта
-set string489=Start File Explorer.
-set string490=Installation failed for WAD:
-set string491=Installation failed for:
-set string492=WAD(s)
-set string493=Pressing any key will open the error log and return to main menu.
+set string489=Запустить проводник.
+set string490=Установка не удалась для WAD:
+set string491=Установка не удалась для:
+set string492=WAD(ы)
+set string493=Нажатие на любую кнопку откроет журнал ошибок и вернёт в главное меню.
