8911f5b9ca65a75cb3284abba25217bebf0fdff2
KcrPL <kcrplkcrpl@gmail.com>
2021-03-01 23:37:20 +0100

    Fix downloading

--- Patchers_Auto_Update/WiiWare-Patcher/patcher.bat
+++ Patchers_Auto_Update/WiiWare-Patcher/patcher.bat
@@ -3,7 +3,7 @@ cls
 echo Please wait...
 set FilesHostedOn=https://patcher.rc24.xyz/update/RiiConnect24-Patcher/v1
 
-curl -s -S --insecure "%FilesHostedOn%/UPDATE/update_assistant.bat" --output "update_assistant.bat"
+curl -f -L -s -S --insecure "%FilesHostedOn%/UPDATE/update_assistant.bat" --output "update_assistant.bat"
 if not "%errorlevel%"=="0" goto download_failed
 update_assistant.bat -WiiWare_Patcher
 exit
