44ad8ebd1aa10a8359dd97d3dd9de9cdb75845b4
KcrPL <kcrplkcrpl@gmail.com>
2021-03-01 23:38:41 +0100

    Also fix RC24 Patcher updating

--- Patchers_Auto_Update/RiiConnect24Patcher/RiiConnect24Patcher.bat
+++ Patchers_Auto_Update/RiiConnect24Patcher/RiiConnect24Patcher.bat
@@ -3,8 +3,18 @@ cls
 echo Please wait...
 set FilesHostedOn=https://kcrPL.github.io/Patchers_Auto_Update/RiiConnect24Patcher
 
-curl -s -S --insecure "%FilesHostedOn%/UPDATE/update_assistant.bat" --output "update_assistant.bat"
-
+curl -f -L -s -S --insecure "%FilesHostedOn%/UPDATE/update_assistant.bat" --output "update_assistant.bat"
+if not "%errorlevel%"=="0" goto download_failed
 update_assistant.bat -RC24_Patcher
 
 exit
+
+
+:download_failed
+cls
+echo Download failed.
+echo Please visit https://github.com/RiiConnect24/RiiConnect24-Patcher/releases to get the latest version.
+echo.
+echo Fatal error, halting.
+pause>NUL
+goto download_failed
