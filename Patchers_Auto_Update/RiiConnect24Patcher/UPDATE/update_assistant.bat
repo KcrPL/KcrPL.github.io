080e4de14d17f41da2ee324846ffee37e4eb119e
KcrPL <kcrplkcrpl@gmail.com>
2021-04-27 01:38:38 +0200

    VFF Downloader for Dolphin Update v1.0.8

--- Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/update_assistant.bat
+++ Patchers_Auto_Update/RiiConnect24Patcher/UPDATE/update_assistant.bat
@@ -1,7 +1,7 @@
 @echo off
 :: ===========================================================================
 :: Update Assistant for RiiConnect24
-set version=1.0.3
+set version=1.0.4
 :: AUTHORS: KcrPL
 :: ***************************************************************************
 :: Copyright (c) 2020 KcrPL, RiiConnect24 and it's (Lead) Developers
@@ -11,8 +11,8 @@ setlocal EnableDelayedExpansion
 
 if exist temp.bat del /q temp.bat
 
-set last_build=2020/11/06
-set at=09:43
+set last_build=2021/04/27
+set at=01:04
 set header=Update Assistant - (C) KcrPL v%version% (Compiled on %last_build% at %at%)
 ::
 set /a no_start=0
@@ -24,6 +24,7 @@ set FilesHostedOn_Beta=https://patcher.rc24.xyz/update/RiiConnect24-Patcher_BETA
 
 if "%1"=="-no_start" set /a no_start=1
 if "%2"=="-no_start" set /a no_start=1
+if "%3"=="-no_start" set /a no_start=1
 
 if "%1"=="-beta" set /a beta=1
 if "%2"=="-beta" set /a beta=1
@@ -37,6 +38,12 @@ if "%3"=="-preboot" set /a preboot=1
 if "%1"=="-RC24_Patcher" goto start_download_rc24_patcher
 if "%2"=="-RC24_Patcher" goto start_download_rc24_patcher
 if "%3"=="-RC24_Patcher" goto start_download_rc24_patcher
+
+if "%1"=="-WiiWare_Patcher" goto start_download_wiiware_patcher
+if "%2"=="-WiiWare_Patcher" goto start_download_wiiware_patcher
+if "%3"=="-WiiWare_Patcher" goto start_download_wiiware_patcher
+
+
 if "%1"=="-VFF_Downloader_Main_Exec" goto start_download_vff_downloader_main_exec
 if "%1"=="-VFF_Downloader_Installer" goto start_download_vff_downloader_install
 if "%1"=="-RC24_DNS_Checker" goto start_download_dns_checker
@@ -56,6 +63,7 @@ echo : Usage: update_assistant.bat [options...]    :
 echo :---------------------------------------------:
 echo.
 echo -RC24_Patcher                  Will download latest RiiConnect24 Patcher to current dir
+echo -WiiWare_Patcher               Will download latest WiiWare Patcher to current dir
 echo -Mail-Patcher                  Will download latest Mail Patcher to current dir
 echo -VFF_Downloader_Main_Exec      Will download executable for VFF Downloader for Dolphin
 echo -VFF_Downloader_Installer      Will download installer for VFF Downloader for Dolphin
@@ -88,7 +96,7 @@ echo %header%
 echo.
 echo -----------------------------------------------------------------------------------------------------------------------------
 echo Please wait! We are now downloading your new VFF Downloader for Dolphin update.
-curl -s -S --insecure "https://kcrpl.github.io/Patchers_Auto_Update/VFF-Downloader-for-Dolphin/UPDATE/Install.bat" --output "InstallTEMP.bat"
+curl -s -S --insecure "https://patcher.rc24.xyz/update/VFF-Downloader-for-Dolphin/v1/UPDATE/Install.bat" --output "InstallTEMP.bat"
 
 set temperrorlev=%errorlevel%
 if not %temperrorlev%==0 goto error_download
@@ -115,7 +123,7 @@ taskkill /im VFF-Downloader-for-DolphinTEMP.exe /f /t
 if exist VFF-Downloader-for-Dolphin.exe del /q VFF-Downloader-for-Dolphin.exe
 ::ren "VFF-Downloader-for-DolphinTEMP.exe" "VFF-Downloader-for-Dolphin.exe"
 
-curl -s -S --insecure "https://kcrpl.github.io/Patchers_Auto_Update/VFF-Downloader-for-Dolphin/UPDATE/VFF-Downloader-for-Dolphin.exe" --output "VFF-Downloader-for-Dolphin.exe"
+curl -s -S --insecure "https://patcher.rc24.xyz/update/VFF-Downloader-for-Dolphin/v1/UPDATE/VFF-Downloader-for-Dolphin.exe" --output "VFF-Downloader-for-Dolphin.exe"
 set temperrorlev=%errorlevel%
 if not %temperrorlev%==0 goto error_download
 
@@ -138,6 +146,27 @@ del patch.bat
 ren "patchTEMP.bat" "patch.bat"
 
 if %no_start%==0 start patch.bat
+
+del /q "%~n0~x0"
+exit
+
+:start_download_wiiware_patcher
+set mode=128,37
+mode %mode%
+cls
+echo %header%
+echo -----------------------------------------------------------------------------------------------------------------------------
+echo.
+echo Please wait! We are now downloading your new WiiWare Patcher update.
+curl -s -S --insecure "https://patcher.rc24.xyz/update/WiiWare-Patcher/v1/UPDATE/WiiWarePatcher.bat" --output "patcherTEMP.bat"
+set temperrorlev=%errorlevel%
+if not %temperrorlev%==0 goto error_download
+
+if exist patcher.bat del patcher.bat
+if exist WiiWarePatcher.bat del WiiWarePatcher.bat
+ren "patcherTEMP.bat" "WiiWarePatcher.bat"
+
+if %no_start%==0 start WiiWarePatcher.bat
 
 del /q "%~n0~x0"
 exit
